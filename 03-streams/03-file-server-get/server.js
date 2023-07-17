8fc68b65b45ea2e643703372f783af12172606b5
mashahori <tmsdesign@yandex.ru>
2023-07-17 14:21:33 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,23 +1,41 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'GET':
+    case "GET":
+      if (pathname.includes("/")) {
+        res.statusCode = 400;
+        res.end("bad request");
+        break;
+      }
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+      stream.on("error", (error) => {
+        if (error.code === "ENOENT") {
+          res.statusCode = 404;
+          res.end("file not found");
+        } else {
+          res.statusCode = 500;
+          res.end("something went wrong");
+        }
+      });
+      req.on("aborted", () => stream.destroy());
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
